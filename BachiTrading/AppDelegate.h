385f0baa8a0dbbf55202013b8ccb6541b9fd55db
bachino90 <emilianobivachi@gmail.com>
2015-11-14 11:43:14 -0300

    Initial Commit

---
+++ BachiTrading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BachiTrading
+//
+//  Created by Emiliano Bivachi on 14/11/15.
+//  Copyright (c) 2015 Emiliano Bivachi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BachiTrading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BachiTrading
+//
+//  Created by Emiliano Bivachi on 14/11/15.
+//  Copyright (c) 2015 Emiliano Bivachi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
